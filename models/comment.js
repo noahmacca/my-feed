4671d2b1ea5c34c91846ea54b87a7a28dc61bfb5
Noah MacCallum <noahmacca@gmail.com>
2017-07-11 22:53:39 -0700

    Add taggedUsers to comment model

--- models/comment.js
+++ models/comment.js
@@ -10,6 +10,15 @@ var commentSchema = mongoose.Schema({
         },
         username: String
     },
+    taggedUsers: [
+        {
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+            username: String
+        }
+    ],
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
